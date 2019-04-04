199413d262d05598334b3bb418be26d9ea8d3c21
snake0 <1260865816@qq.com>
2019-04-04 23:14:39 +0800

    resign

---
+++ signer.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+if len(sys.argv) != 3:
+    print("usage: python signer.py [apksigner.jar dir] [*.apk dir]")
+    sys.exit(0)
+
+cmd = "java -jar %s sign --ks keys/hellokey.jks %s" % (sys.argv[1], sys.argv[2])
+os.system(cmd)
+print("resign verify:\n")
+cmd = "java -jar %s verify -v %s" % (sys.argv[1], sys.argv[2])
+os.system(cmd)
